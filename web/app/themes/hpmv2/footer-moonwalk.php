634e83860184f53501b6fb05a7be23005fa2cf91
Jared Counts <jcounts@houstonpublicmedia.org>
2019-06-26 23:09:09 -0500

    Adding Moonwalk template

---
+++ web/app/themes/hpmv2/footer-moonwalk.php
@@ -0,0 +1,62 @@
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info">
+					<div class="foot-logo">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" title="<?php bloginfo( 'name' ); ?>"><img src="https://cdn.hpm.io/assets/images/KO_1Line.png" alt="<?php bloginfo( 'name' ); ?>" /></a>
+					</div>
+					<div id="footer-social">
+						<div class="footer-social-icon footer-facebook">
+							<a href="https://www.facebook.com/houstonpublicmedia" target="_blank"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+						</div>
+						<div class="footer-social-icon footer-twitter">
+							<a href="https://twitter.com/houstonpubmedia" target="_blank"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+						</div>
+						<div class="footer-social-icon footer-instagram">
+							<a href="https://instagram.com/houstonpubmedia" target="_blank"><span class="fa fa-instagram" aria-hidden="true"></span></a>
+						</div>
+						<div class="footer-social-icon footer-youtube">
+							<a href="https://www.youtube.com/user/houstonpublicmedia" target="_blank"><span class="fa fa-youtube-play" aria-hidden="true"></span></a>
+						</div>
+						<div class="footer-social-icon footer-soundcloud">
+							<a href="https://soundcloud.com/houston-public-media" target="_blank"><span class="fa fa-soundcloud" aria-hidden="true"></span></a>
+						</div>
+					</div>
+					<nav id="secondary-navigation" class="footer-navigation" role="navigation">
+<?php
+	wp_nav_menu( array(
+		'menu_class' => 'nav-menu',
+		'theme_location' => 'footer',
+		'walker' => new HPMv2_Menu_Walker
+	) );
+?>
+						<div class="clear"></div>
+					</nav>
+					<p>Houston Public Media is supported with your gifts to the Houston Public Media Foundation and is licensed to the <a href="http://www.uh.edu" target="_blank" style="color: #cc0000;">University of Houston</a></p>
+					<p>Copyright &copy; <?php echo date('Y'); ?> | <a href="http://www.uhsystem.edu/privacy-notice/">Privacy
+							Policy</a></p>
+				</div><!-- .site-info -->
+			</footer><!-- .site-footer -->
+		</div><!-- .site -->
+		<script type='text/javascript'>
+			var _sf_async_config={};
+			/** CONFIGURATION START **/
+			_sf_async_config.uid = 33583;
+			_sf_async_config.domain = 'houstonpublicmedia.org';
+			_sf_async_config.useCanonical = true;
+			(function(){
+				function loadChartbeat() {
+					window._sf_endpt=(new Date()).getTime();
+					var e = document.createElement('script');
+					e.setAttribute('language', 'javascript');
+					e.setAttribute('type', 'text/javascript');
+					e.setAttribute('src', '//static.chartbeat.com/js/chartbeat.js');
+					document.body.appendChild(e);
+				}
+				var oldonload = window.onload;
+				window.onload = (typeof window.onload != 'function') ?
+					loadChartbeat : function() { oldonload(); loadChartbeat(); };
+			})();
+		</script>
+	</body>
+</html>
+
+
---
+++ web/app/themes/hpmv2/page-moonwalk.php
@@ -0,0 +1,244 @@
+<?php
+/*
+Template Name: Moonwalk
+*/
+	get_header( 'moonwalk' );
+	$audio = new WP_Query([
+		'category_name' => 'moonwalk',
+		'orderby' => 'date',
+		'order'   => 'ASC',
+		'posts_per_page' => -1,
+		'post_status' => [ 'publish' ],
+		'ignore_sticky_posts' => 1
+	]);
+	$first = $audio->posts[0];
+	$first_meta = get_post_meta( $first->ID, 'hpm_podcast_enclosure', true );
+	function hpm_youtube_playlist_hah( $key ) {
+		$list = get_transient( 'hpm_yt_'.$key );
+		if ( !empty( $list ) ) :
+			return $list;
+		endif;
+		$remote = wp_remote_get( esc_url_raw( 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId='.$key.'&key=AIzaSyBHSGTRPfGElaMTniNCtHNbHuGHKcjPRxw' ) );
+		if ( is_wp_error( $remote ) ) :
+			return false;
+		else :
+			$yt = wp_remote_retrieve_body( $remote );
+			$json = json_decode( $yt, TRUE );
+			$items = $json['items'];
+			set_transient( 'hpm_yt_'.$key, $items, 300 );
+			return $items;
+		endif;
+	}
+	$json = hpm_youtube_playlist_hah( 'PLGHyNdqkLN-ABBYbvKmMw7tGt_0eeALR-' );
+	$titles = [
+		'The Mission',
+		'The Women Of Mission Control',
+		'Breaking Barriers',
+		'Generations',
+		'The Next Adventure'
+	]; ?>
+				<div id="primary" class="content-area">
+					<main id="main" class="site-main" role="main">
+						<header class="page-header">
+							<div class="header-logo">
+								<a href="/" rel="home" title="Houston Public Media homepage"><img src="https://local.hpm.io/moon/HPM_Logo.svg" alt="Houston Public Media, a service of the University of Houston" /></a>
+							</div>
+							<div class="tch-wrap">
+								<a href="http://www.texaschildrens.org/best" title="Presenting Sponsor: Texas Children's Hospital" target="_blank">Presenting Sponsor</a>
+							</div>
+						</header><!-- .entry-header -->
+						<div class="page-content" id="main-content">
+							<section id="moon-desc">
+								<div class="full-wrap">
+									<h2>Fifty years after Apollo 11, Houston Public Media shares memories of the Apollo missions and human stories that followed the Space Age.</h2>
+									<p>This July experience Moonwalk, a multi-platform, multi-generational series of engaging conversations between voices in aerospace – including an Apollo 15 astronaut, the first African American to perform a spacewalk, present-day NASA astronauts, students from the Mars generation and the first female engineer in mission control during the Apollo Era.</p>
+								</div>
+							</section>
+							<section id="moon-video">
+								<div class="full-wrap">
+									<div class="moon-wrap">
+										<a href="#" class="readmore" id="video-more"><i class="fa fa-indent" aria-hidden="true"></i> See all videos</a>
+										<div id="youtube-player" style="background-image: url( '<?php echo $json[0]['snippet']['thumbnails']['high']['url']; ?>' );" data-ytid="<?php echo $json[0]['snippet']['resourceId']['videoId']; ?>" data-yttitle="<?php echo htmlentities( $tubes['snippet']['title'], ENT_COMPAT ); ?>">
+											<span class="fa fa-play" id="play-button"></span>
+										</div>
+										<div class="moon-video-info">
+											<h3 id="moon-yt-title"><?php echo $titles[0]; ?></h3>
+											<p id="moon-yt-desc"><?php echo wp_trim_words( htmlentities( $json[0]['snippet']['description'] ), 50, '...' ); ?></p>
+										</div>
+									</div>
+									<aside id="videos-nav">
+										<nav id="videos">
+											<div class="videos-playlist">
+												<p>Moonwalk Videos</p>
+												<div id="videos-close" class="playlist-close"><span class="fa fa-close"></span></div>
+											</div>
+											<ul>
+<?php foreach ( $json as $k => $tubes ) : ?>
+												<li id="<?php echo $tubes['snippet']['resourceId']['videoId']; ?>" data-ytid="<?php echo $tubes['snippet']['resourceId']['videoId']; ?>" data-yttitle="<?php echo $titles[$k]; ?>" data-ytdesc="<?php echo wp_trim_words( htmlentities( $tubes['snippet']['description'] ), 50, '...' ); ?>"<?PHP echo ( $k == 0 ? ' class="current"' : '' ); ?>>
+													<div class="videos-thumbnail"><img src="<?php echo $tubes['snippet']['thumbnails']['medium']['url']; ?>" alt="<?php echo $titles[$k]; ?>" /></div>
+													<div class="videos-info"><?php echo $titles[$k]; ?></div>
+												</li>
+<?php endforeach; ?>
+										
+											</ul>
+										</nav>
+									</aside>
+								</div>
+							</section>
+							<section id="moon-podcast">
+								<div class="full-wrap">
+									<h2>Listen to Moonwalk Podcast</h2>
+									<div id="jquery_jplayer_1" class="jp-jplayer"></div>
+									<div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
+										<div class="moon-wrap">
+											<p>Subscribe and listen to full-length versions of our video stories</p>
+											<p>&nbsp;</p>
+											<ul class="pod-sub">
+												<li>
+													<a href="https://itunes.apple.com/us/podcast/stories-from-the-storm/id1406912537" target="_blank" title="Subscribe on Apple Podcasts"><img src="https://cdn.hpm.io/assets/images/harvey/apple_pod.png" alt="Subscribe on Apple Podcasts" /></a>
+												</li>
+												<li>
+													<a href="https://playmusic.app.goo.gl/?ibi=com.google.PlayMusic&isi=691797987&ius=googleplaymusic&apn=com.google.android.music&link=https://play.google.com/music/m/Iewfxybo66acwbsqei5lqe3zncy?t%3DStories_from_the_Storm%26pcampaignid%3DMKT-na-all-co-pr-mu-pod-16" target="_blank" title="Subscribe on Google Podcasts"><img src="https://cdn.hpm.io/assets/images/harvey/google_pod.png" alt="Subscribe on Google Podcasts" /></a>
+												</li>
+											</ul>
+											<div class="podcast-image">
+												<img src="https://local.hpm.io/moon/moonwalk_header-small.jpg" alt="Moonwalk podcast" />
+											</div>
+											<a href="#" class="readmore" id="pod-more"><i class="fa fa-indent" aria-hidden="true"></i> All episodes</a>
+											<div class="jp-type-single">
+												<div class="jp-gui jp-interface">
+													<div class="jp-progress-wrapper">
+														<div class="jp-progress">
+															<div class="jp-seek-bar">
+																<div class="jp-play-bar"></div>
+															</div>
+														</div>
+														<div class="jp-time-holder">
+															<div class="jp-current-time" role="timer" aria-label="time"></div>
+															<div class="jp-duration" role="timer" aria-label="duration"></div>
+														</div>
+													</div>
+													<div class="jp-controls">
+														<button class="jp-previous" role="button" tabindex="0">
+															<span class="fa fa-step-backward" aria-hidden="true"></span>
+															<span class="screen-reader-text">Previous</span>
+														</button>
+														<button class="jp-play" role="button" tabindex="0">
+															<span class="fa fa-play" aria-hidden="true"></span>
+															<span class="screen-reader-text">Play</span>
+														</button>
+														<button class="jp-pause" role="button" tabindex="0">
+															<span class="fa fa-pause" aria-hidden="true"></span>
+															<span class="screen-reader-text">Pause</span>
+														</button>
+														<button class="jp-next" role="button" tabindex="0">
+															<span class="fa fa-step-forward" aria-hidden="true"></span>
+															<span class="screen-reader-text">Next</span>
+														</button>
+														<button class="jp-mute" role="button" tabindex="0">
+															<span class="screen-reader-text">Mute</span>
+														</button>
+													</div>
+												</div>
+											</div>
+										</div>
+										<aside id="pod-nav">
+											<nav id="pod">
+												<div class="pod-playlist">
+													<p>Moonwalk Podcast</p>
+													<div id="pod-close" class="playlist-close"><span class="fa fa-close"></span></div>
+												</div>
+												<div class="jp-playlist">
+													<ul>
+														<li></li>
+													</ul>
+												</div>
+											</nav>
+										</aside>
+									</div>
+									<h3>Listen to the <span class="special">special edition Houston Matters</span> on <span class="special">July 1–8</span></h3>
+								</div>
+							</section>
+							<section id="moon-look">
+								<div class="full-wrap">
+									<h2>A Look Back at the Space Age</h2>
+									<p>Check out these small clips in the KUHT archive about space exploration in the past.</p>
+									<article>
+										<p class="youtube-wrap"><iframe width="560" height="315" src="https://www.youtube.com/embed/Yv0Q36pjDoM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
+										<h2><strong>KUHT Flashback</strong>: 1962 Interview With Tech Working On Apollo Simulator Model</h2>
+									</article>
+									<article>
+										<p class="youtube-wrap"><iframe width="560" height="315" src="https://www.youtube.com/embed/7Fuqh7Xj1zg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
+										<h2><strong>KUHT Flashback</strong>: 1962 Report On Plans For NASA’s “Manned Space Center”</h2>
+									</article>
+									<article>
+										<p class="youtube-wrap"><iframe width="560" height="315" src="https://www.youtube.com/embed/VJFir6uanW8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
+										<h2><strong>KUHT Flashback</strong>: 1962 Report On People Relocating To Houston To Work For NASA</h2>
+									</article>
+								</div>
+							</section>
+							<section id="moon-behind">
+								<h2>Behind the Scenes</h2>
+								<p>Take a behind-the-scenes look at the making of <em>Moonwalk</em></p>
+								<p class="explore-button"><a href="https://www.flickr.com/photos/houstonpubmedia/albums/72157709085401958" target="_blank">Explore</a></p>
+							</section>
+						</div><!-- .entry-content -->
+					</main><!-- .site-main -->
+				</div><!-- .content-area -->
+			</div><!-- .site-content -->
+<?php 
+$eps = [];
+$c = 0;
+if ( $audio->have_posts() ) :
+	while ( $audio->have_posts() ) :
+		$audio->the_post();
+		$id = get_the_ID();
+		$enclose = get_post_meta( $id, 'hpm_podcast_enclosure', true );
+		if ( $c == 0 ) :
+			$first = [
+				'title' => get_the_title(),
+				'url' => $enclose['url']
+			];
+		endif;
+		$eps[] = '{title: "'.get_the_title().'",artist:"",mp3:"'.$enclose['url'].'"}';
+		$c++;
+	endwhile;
+endif;
+wp_reset_postdata(); ?>
+			<script>
+				jQuery(document).ready(function($){
+					$("#jquery_jplayer_1").jPlayer({
+						ready: function () {
+							$(this).jPlayer("setMedia", {
+								title: "<?php echo $first['title']; ?>",
+								mp3: "<?php echo $first['url']; ?>?source=jplayer-article"
+							});
+						},
+						swfPath: "https://cdn.hpm.io/assets/js/jplayer",
+						supplied: "mp3",
+						preload: "metadata",
+						cssSelectorAncestor: "#jp_container_1",
+						wmode: "window",
+						useStateClassSkin: true,
+						autoBlur: false,
+						smoothPlayBar: true,
+						keyEnabled: true,
+						remainingDuration: true,
+						toggleDuration: false,
+						muted: false
+					});
+					var myPlaylist = new jPlayerPlaylist({
+						jPlayer: "#jquery_jplayer_1",
+						cssSelectorAncestor: "#jp_container_1"
+					}, [<?php echo implode( ',', $eps ); ?>], {
+						playlistOptions: {
+							enableRemoveControls: false
+						}
+					});
+					$("#jquery_jplayer_1").bind( $.jPlayer.event.play, function(event) {
+						console.log('Close Podcast Nav');
+						$('#pod-nav').removeClass('nav-active');
+					});
+				});
+			</script>
+<?php get_footer( 'moonwalk' ); ?>
