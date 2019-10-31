547db8bcaa4edfb79cb4033262e0f5f15755633c
Jared Counts <jcounts@houstonpublicmedia.org>
2019-10-31 11:45:59 -0500

    Removing LudicrousDB setup

--- web/app/db.php
+++ web/app/db.php
@@ -1,40 +1,18 @@
 <?php
-
 /**
- * Plugin Name: LudicrousDB
- * Plugin URI:  https://wordpress.org/plugins/ludicrousdb/
- * Author:      John James Jacoby
- * Author URI:  https://jjj.blog
- * License:     GPL v2 or later
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: ludicrousdb
- * Version:     4.1.0
- * Description: An advanced database interface for WordPress that supports replication, fail-over, load balancing, and partitioning
+ * Plugin Name: Query Monitor Database Class
+ *
+ * *********************************************************************
+ *
+ * Ensure this file is symlinked to your wp-content directory to provide
+ * additional database query information in Query Monitor's output.
+ *
+ * *********************************************************************
+ *
+ * @package query-monitor
  */
 
-// Exit if accessed directly
-defined( 'ABSPATH' ) || exit;
-
-// Custom directory name
-$ldb_dirname     = defined( 'LDB_DIRNAME'     ) ? LDB_DIRNAME     : 'ludicrousdb';
-
-// Supported plugin directories
-$wp_plugin_dir   = defined( 'WP_PLUGIN_DIR'   ) ? WP_PLUGIN_DIR   : WP_CONTENT_DIR . '/plugins';
-$wpmu_plugin_dir = defined( 'WPMU_PLUGIN_DIR' ) ? WPMU_PLUGIN_DIR : WP_CONTENT_DIR . '/mu-plugins';
-$wpdb_plugin_dir = defined( 'WPDB_PLUGIN_DIR' ) ? WPDB_PLUGIN_DIR : WP_CONTENT_DIR . '/db-plugins';
-
-// Check /plugins/
-if ( file_exists( "{$wp_plugin_dir}/{$ldb_dirname}/ludicrousdb.php" ) ) {
-	require_once "{$wp_plugin_dir}/{$ldb_dirname}/ludicrousdb.php";
-
-// Check /mu-plugins/
-} elseif ( file_exists( "{$wpmu_plugin_dir}/{$ldb_dirname}/ludicrousdb.php" ) ) {
-	require_once "{$wpmu_plugin_dir}/{$ldb_dirname}/ludicrousdb.php";
-
-// Check /db-plugins/
-} elseif ( file_exists( "{$wpdb_plugin_dir}/{$ldb_dirname}/ludicrousdb.php" ) ) {
-	require_once "{$wpdb_plugin_dir}/{$ldb_dirname}/ludicrousdb.php";
-}
+defined( 'ABSPATH' ) || die();
 
 if ( defined( 'QM_DISABLED' ) && QM_DISABLED ) {
 	return;
@@ -52,8 +30,7 @@ if ( defined( 'DOING_CRON' ) && DOING_CRON ) {
 }
 
 # No autoloaders for us. See https://github.com/johnbillion/query-monitor/issues/7
-// $qm_dir = dirname( dirname( __FILE__ ) );
-$qm_dir = $wp_plugin_dir . '/query-monitor';
+$qm_dir = dirname( dirname( __FILE__ ) );
 $plugin = "{$qm_dir}/classes/Plugin.php";
 
 if ( ! is_readable( $plugin ) ) {
