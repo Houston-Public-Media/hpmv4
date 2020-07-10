c4970843e911fae49f58b50922b79273147c8b79
Jared Counts <jcounts@houstonpublicmedia.org>
2020-07-10 16:18:18 -0500

    Adding filter for non-UTF-8 characters in page content

--- web/app/themes/hpmv2/functions.php
+++ web/app/themes/hpmv2/functions.php
@@ -549,3 +549,39 @@ function hpm_yt_embed_mod( $content ) {
 	return $content;
 }
 add_filter( 'the_content', 'hpm_yt_embed_mod', 999 );
+
+function hpm_charset_clean( $content ) {
+	$find = [ ' ', '…', '’', '“', '”' ];
+	$replace = [ '', '...', "'", '"', '"' ];
+	return str_replace( $find, $replace, $content );
+}
+add_filter( 'the_content', 'hpm_charset_clean', 10 );
+
+function hpm_revue_signup( $content ) {
+	global $post;
+	if ( is_single() && $post->post_type == 'post' ) :
+		if ( in_category( 'news' ) ) :
+			$content .= "<div id=\"revue-embed\">
+	<form action=\"https://www.getrevue.co/profile/HoustonPublicMediaNewsroom/add_subscriber\" method=\"post\" id=\"revue-form\" name=\"revue-form\"  target=\"_blank\">
+		<div class=\"revue-form-group\">
+			<label for=\"member_email\">Email address</label>
+			<input class=\"revue-form-field\" placeholder=\"Your email address...\" type=\"email\" name=\"member[email]\" id=\"member_email\">
+		</div>
+		<div class=\"revue-form-group\">
+			<label for=\"member_first_name\">First name <span class=\"optional\">(Optional)</span></label>
+			<input class=\"revue-form-field\" placeholder=\"First name... (Optional)\" type=\"text\" name=\"member[first_name]\" id=\"member_first_name\">
+		</div>
+		<div class=\"revue-form-group\">
+			<label for=\"member_last_name\">Last name <span class=\"optional\">(Optional)</span></label>
+			<input class=\"revue-form-field\" placeholder=\"Last name... (Optional)\" type=\"text\" name=\"member[last_name]\" id=\"member_last_name\">
+		</div>
+		<div class=\"revue-form-actions\">
+			<input type=\"submit\" value=\"Subscribe\" name=\"member[subscribe]\" id=\"member_submit\">
+		</div>
+	</form>
+</div>";
+		endif;
+	endif;
+	return $content;
+}
+//add_filter( 'the_content', 'hpm_revue_signup', 8 );
