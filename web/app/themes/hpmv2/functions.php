9d43a895ae2c9f4883401d043368d8a673e6d87b
Jared Counts <jcounts@houstonpublicmedia.org>
2020-10-19 09:20:43 -0500

    Fixed AppleNews plugin filter for dev environments

--- web/app/themes/hpmv2/functions.php
+++ web/app/themes/hpmv2/functions.php
@@ -602,11 +602,8 @@ function hpm_footer_ads() {
 }
 add_action( 'wp_footer', 'hpm_footer_ads', 100 );
 
-function skip_apple_news( $post_id ) {
-	if ( WP_ENV === 'production' ) :
-		return true;
-	else :
-		return false;
+function skip_apple_news( $post_id, $post ) {
+	if ( WP_ENV !== 'production' ) :
+		apply_filters( 'apple_news_skip_push', true, $post_id );
 	endif;
 }
-add_filter( 'apple_news_skip_push', 'skip_apple_news', 10, 1 );
