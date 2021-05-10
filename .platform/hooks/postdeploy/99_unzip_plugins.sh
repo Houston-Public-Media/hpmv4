c5040bd71723de04bee80ea5f94280b7923c5d68
Jared Counts <jcounts@houstonpublicmedia.org>
2021-05-10 16:42:11 -0500

    Added command to make platform hooks executable

--- .platform/hooks/postdeploy/99_unzip_plugins.sh
+++ .platform/hooks/postdeploy/99_unzip_plugins.sh
@@ -2,4 +2,4 @@
 unzip -qo /var/app/plugins.zip -d "/var/app/current/web/app/plugins" -x "__MACOSX/*"
 chmod -R 777 /var/app/current/web/app/uploads
 rm -rf /var/app/current/web/wp/wp-content/themes/twenty*
-#cp /var/app/.env /var/app/current/.env
+cp /var/app/.env /var/app/current/.env
