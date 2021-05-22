03ce0a529559ea230bffc79ea752e5bde3cb0bc0
Jared Counts <jcounts@houstonpublicmedia.org>
2021-05-22 12:36:40 -0500

    Put social icons back

--- web/app/themes/hpmv2/includes/head.php
+++ web/app/themes/hpmv2/includes/head.php
@@ -17,7 +17,7 @@ function hpm_site_header() { ?>
 					<div id="top-listen"><button data-href="/listen-live" data-dialog="480:855"><span class="fas fa-microphone" aria-hidden="true"></span>Listen</button></div>
 					<div id="top-watch"><button data-href="/watch-live" data-dialog="820:850"><span class="fas fa-tv" aria-hidden="true"></span>Watch</button></div>
 					<div id="top-donate"><a href="/donate"><span class="fas fa-heart" aria-hidden="true"></span><br /><span class="top-mobile-text">Donate</span></a></div>
-					<?php /* ?><div id="header-social">
+					<?php  ?><div id="header-social">
 						<div class="header-social-icon header-facebook">
 							<a href="https://www.facebook.com/houstonpublicmedia" target="_blank"><span class="fab fa-facebook-f" aria-hidden="true"></span></a>
 						</div>
@@ -33,7 +33,7 @@ function hpm_site_header() { ?>
 						<div class="header-social-icon header-linkedin">
 							<a href="https://linkedin.com/company/houstonpublicmedia" target="_blank"><span class="fab fa-linkedin-in" aria-hidden="true"></span></a>
 						</div>
-					</div><?php */ ?>
+					</div><?php  ?>
 					<div id="top-mobile-menu"><span class="fas fa-bars" aria-hidden="true"></span><br /><span class="top-mobile-text">Menu</span></div>
 					<nav id="site-navigation" class="main-navigation" role="navigation">
 						<div id="top-search"><span class="fas fa-search" aria-hidden="true"></span><?php get_search_form(); ?></div>
