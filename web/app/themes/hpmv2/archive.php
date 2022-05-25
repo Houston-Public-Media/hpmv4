3e7ac2c48779fda8b2fab40f17ce7fe1a58009cb
Jared Counts <asentenceismyname@gmail.com>
2022-05-25 13:03:11 -0500

    Plugin updates, inDepth redirect fix Old content.php removal

--- web/app/themes/hpmv2/archive.php
+++ web/app/themes/hpmv2/archive.php
@@ -42,11 +42,11 @@ if ( is_category() ) :
 				wp_reset_postdata();
 			endif;
 		endif;
-		if ( $cat->term_id == 29328 ) :
-			header( "HTTP/1.1 301 Moved Permanently" );
-			header( 'Location: /news/indepth/' );
-			exit;
-		endif;
+	endif;
+	if ( $cat->term_id == 29328 ) :
+		header( "HTTP/1.1 301 Moved Permanently" );
+		header( 'Location: /news/indepth/' );
+		exit;
 	endif;
 endif;
 get_header(); ?>
