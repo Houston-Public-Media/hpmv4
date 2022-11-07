d5d38d894d91718eff2621962891445b24e1fea7
Jared Counts <asentenceismyname@gmail.com>
2022-11-07 14:43:39 -0600

    Texas Mutual Bauer Update

--- web/app/themes/hpmv4/single.php
+++ web/app/themes/hpmv4/single.php
@@ -112,7 +112,7 @@ if ( is_preview() ) : ?>
 				elseif ( $cats->term_id == 12 ) : ?>
 				<div class="sidebar-ad">
 					<h4>Support Comes From</h4>
-					<p><a href="https://www.texasmutual.com/employers/pr/safer-stronger-better?utm_source=Digital+banners&utm_medium=HoustonPublicMedia.org&utm_campaign=BIB&utm_id=EmployerBIB"><img src="https://cdn.hpm.io/assets/images/EMPL-DIVIDENDS-2022_300x250_ALT-1.jpg.webp" alt="Protecting Texas businesses and workers for 30 years." /></a></p>
+					<p><a href="https://www.texasmutual.com/employers/pr/business-is-better?utm_source=Bauer+Biz&utm_medium=Digital&utm_campaign=BIB-Claims&utm_id=BIB-Claims"><img src="https://cdn.hpm.io/assets/images/BIB_CLAIMS_300x250-PBS-Texas-Mutual-BIB-Static-Backup.jpg.webp" alt="Protecting Texas businesses and workers for 30 years." /></a></p>
 				</div>
 			<?php
 				endif;
