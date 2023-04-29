71662020323e39be082d2dc29e7ee46910a6cb2e
Jared Counts <asentenceismyname@gmail.com>
2023-04-28 22:18:12 -0500

    Fixed Google Tag Services errors

--- web/app/themes/hpmv4/includes/google.php
+++ web/app/themes/hpmv4/includes/google.php
@@ -11,22 +11,22 @@ function hpm_google_tracker(): void {
 				googletag.defineSlot('/9147267/HPM_Kids_Sidebar', [300, 250], 'div-gpt-ad-1467299583216-2').addService(googletag.pubads());
 				googletag.defineSlot('/9147267/HPM_Kids_Sidebar', [300, 250], 'div-gpt-ad-1467299583216-3').addService(googletag.pubads());
 				var dfpWide = window.innerWidth;
-				if ( dfpWide > 1000 ) {
-					googletag.defineSlot('/9147267/HPM_Under_Nav', [970, 50], 'div-gpt-ad-1488818411584-0').addService(googletag.pubads());
-					document.getElementById('div-gpt-ad-1488818411584-0').style.width = '970px';
-				}
-				else if ( dfpWide <= 1000 && dfpWide > 730 ) {
-					googletag.defineSlot('/9147267/HPM_Under_Nav', [728, 90], 'div-gpt-ad-1488818411584-0').addService(googletag.pubads());
-					document.getElementById('div-gpt-ad-1488818411584-0').style.width = '728px';
-				}
-				else if ( dfpWide <= 730 ) {
-					googletag.defineSlot('/9147267/HPM_Under_Nav', [320, 50], 'div-gpt-ad-1488818411584-0').addService(googletag.pubads());
-					document.getElementById('div-gpt-ad-1488818411584-0').style.width = '320px';
+				var dfpUnderNav = document.getElementById('div-gpt-ad-1488818411584-0');
+				if (dfpUnderNav !== null) {
+					if (dfpWide > 1000) {
+						googletag.defineSlot('/9147267/HPM_Under_Nav', [970, 50], 'div-gpt-ad-1488818411584-0').addService(googletag.pubads());
+						dfpUnderNav.style.width = '970px';
+					} else if (dfpWide <= 1000 && dfpWide > 730) {
+						googletag.defineSlot('/9147267/HPM_Under_Nav', [728, 90], 'div-gpt-ad-1488818411584-0').addService(googletag.pubads());
+						dfpUnderNav.style.width = '728px';
+					} else if (dfpWide <= 730) {
+						googletag.defineSlot('/9147267/HPM_Under_Nav', [320, 50], 'div-gpt-ad-1488818411584-0').addService(googletag.pubads());
+						dfpUnderNav.style.width = '320px';
+					}
 				}
 				googletag.defineSlot('/9147267/HPM_Music_Sidebar', [300, 250], 'div-gpt-ad-1470409396951-0').addService(googletag.pubads());
 				googletag.defineSlot('/9147267/HPM_Support_Sidebar', [300, 250], 'div-gpt-ad-1394579228932-1').addService(googletag.pubads());
 				googletag.defineSlot('/9147267/HPM_Support_Sidebar', [300, 250], 'div-gpt-ad-1394579228932-2').addService(googletag.pubads());
-				//googletag.pubads().collapseEmptyDivs();
 				googletag.defineSlot('/9147267/HPM_About_300x250', [300, 250], 'div-gpt-ad-1579034137004-0').addService(googletag.pubads());
 				googletag.pubads().addEventListener('slotRenderEnded', function(event) {
 					var slotId = event.slot.getSlotElementId();
