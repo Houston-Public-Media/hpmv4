074975f0cf526676d2e84206b014a2d40135a190
Jared Counts <asentenceismyname@gmail.com>
2023-05-16 16:30:08 -0500

    Refinement to job linker

--- web/app/themes/hpmv4/includes/shortcodes.php
+++ web/app/themes/hpmv4/includes/shortcodes.php
@@ -607,6 +607,7 @@ function hpm_careers_trans(): string {
 		set_transient( 'hpm_careers', $output, 900 );
 		return $output;
 	}
+	$output .= '<svg hidden xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><use href="#hpm-job-link"></use><symbol id="hpm-job-link"><path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/></symbol></svg>';
 	foreach ( $json['requisitionList'] as $j ) {
 		if ( !in_array( $j['contestNo'], $desc['exclude'] ) ) {
 			if ( !empty( $desc[ $j['contestNo'] ]['title'] ) ) {
@@ -614,7 +615,7 @@ function hpm_careers_trans(): string {
 			} else {
 				$title = trim( $j['column'][0] );
 			}
-			$output .= '<details id="' . $j['contestNo'] . '"><summary>' . $title . '</strong></summary><div class="job-link" title="Click for a direct link to this job posting" data-job="#' . $j['contestNo'] . '">🔗</div>';
+			$output .= '<details id="' . $j['contestNo'] . '"><summary>' . $title . '</strong></summary><div class="job-link" title="Click for a direct link to this job posting" data-job="#' . $j['contestNo'] . '"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><use href="#hpm-job-link"></use></svg></div>';
 			if ( !empty( $desc[ $j['contestNo'] ]['description'] ) ) {
 				$output .= $desc[ $j['contestNo'] ]['description'];
 			}
