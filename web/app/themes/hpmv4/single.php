5164d3771ca87097f65e711495090697aadc502d
Smita Patel <sapate34@central.uh.edu>
2024-01-10 15:06:11 -0600

    updated newsletter signup form on story page

--- web/app/themes/hpmv4/single.php
+++ web/app/themes/hpmv4/single.php
@@ -131,17 +131,17 @@ if ( is_preview() ) { ?>
 		</aside>
 
 
-        <!--<div class="newsletter-form">
+        <div class="newsletter-form">
             <?php
-/*            if ( is_single() && get_post_type() == 'post' ) {
+            if ( is_single() && get_post_type() == 'post' ) {
                 if ( in_category( 'news' ) ) {
                     $form_id = '441232';
                     echo '<div id="revue-embed">' . do_shortcode( '[wpforms id="' . $form_id . '" title="true" description="true"]' ) . '</div>';
                 }
             }
 
-            */?>
-        </div>-->
+            ?>
+        </div>
 
 	</main>
 </div>
