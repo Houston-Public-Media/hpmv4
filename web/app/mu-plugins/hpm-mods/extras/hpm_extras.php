3dd90bee979c6f1ccbada8baf9dece8a87825f67
Smita Patel <sapate34@central.uh.edu>
2024-02-02 13:45:34 -0600

    Removed Thumbnails from Most Viewed section

--- web/app/mu-plugins/hpm-mods/extras/hpm_extras.php
+++ web/app/mu-plugins/hpm-mods/extras/hpm_extras.php
@@ -448,7 +448,7 @@ function analyticsPull_update(): void {
 				$imageBlock = get_the_post_thumbnail( $match[1], 'thumbnail' );
 			}
 
-			$output .= '<li><a href="' . $row->getDimensionValues()[0]->getValue() . '" rel="bookmark"><span>' . $title . '</span>' . $imageBlock . '</a></li>';
+			$output .= '<li><a href="' . $row->getDimensionValues()[0]->getValue() . '" rel="bookmark"><span>' . $title . '</span></a></li>';
 		}
 	}
 	$output .= "</ul>";
