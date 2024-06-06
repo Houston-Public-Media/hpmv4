21d51eeb8f54c0ec4da17d8342978bbfe516a093
Jared Counts <asentenceismyname@gmail.com>
2024-06-06 11:38:49 -0500

    Redirect all podcast feeds to CDN Change homepage NPR pull to 1002 collection ID

--- web/app/themes/hpmv4/front-page.php
+++ web/app/themes/hpmv4/front-page.php
@@ -226,7 +226,7 @@ $tras = null; ?>
 					<h2 class="title">
 						<strong>News from <span>NPR</span></strong>
 					</h2>
-					<?php echo hpm_nprapi_output(); ?>
+					<?php echo hpm_nprapi_output( 1002 ); ?>
 				</div>
 				<div class="col-sm-12 col-lg-4 news-list-right most-view homepage-desktop-gdc hidden">
 					<h2 class="title title-full">
