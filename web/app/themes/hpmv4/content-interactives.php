944a8262e136b4f8350cfd5fb9279d1930fedd1e
Smita Patel <sapate34@central.uh.edu>
2024-07-22 16:02:35 -0500

    Added HPM Interactives module on home page.

---
+++ web/app/themes/hpmv4/content-interactives.php
@@ -0,0 +1,70 @@
+
+<section class="section">
+    <h2 class="title">
+        <strong>HOUSTON PUBLIC MEDIA'S <span>INTERACTIVES</span></strong>
+    </h2>
+    <div class="row">
+        <div class="col-sm-3">
+                <div class="card mb-3">
+                    <div class="card-header">
+                        <a style="text-decoration: none; color:#fff;" href="https://www.houstonpublicmedia.org/articles/news/hurricane/2024/05/30/489073/hurricane-tracker-houston-texas-united-states/">Hurricane & Tropical Strom Tracker</a>
+                    </div>
+                    <div class="row g-0">
+                        <div class="col-md-12">
+                            <a href="https://www.houstonpublicmedia.org/articles/news/hurricane/2024/05/30/489073/hurricane-tracker-houston-texas-united-states/"><img src="https://cdn.houstonpublicmedia.org/assets/images/Hurricane-and-storm-tracker-hpm-interactives.png.webp" alt="Hurricane and Tropical Storm Tracker" style="padding: 6px;" /></a>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        <div class="col-sm-3">
+            <div class="card mb-3">
+                <div class="card-header">
+                    <a style="text-decoration: none; color:#fff;" href="http://172.21.136.142/texas-power-outage-tracker-map//">Texas Power Outage Tracker Map</a>
+                </div>
+                <div class="row g-0">
+                    <div class="col-md-12">
+                        <a href="http://172.21.136.142/texas-power-outage-tracker-map/"><img src="https://cdn.houstonpublicmedia.org/assets/images/texas-power-outage-tracker-map.png.webp" alt="Texas Power Outage Tracker Map" style="padding: 6px;" /></a>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card mb-3">
+                <div class="card-header">
+                    <a style="text-decoration: none; color:#fff;" href="http://172.21.136.142/articles/news/2024/06/11/476089/heat-tracker-2024/">Heat Tracker Map</a>
+                </div>
+                <div class="row g-0">
+                    <div class="col-md-12">
+                        <a href="http://172.21.136.142/articles/news/2024/06/11/476089/heat-tracker-2024/">
+                            <img src="https://cdn.houstonpublicmedia.org/assets/images/Heat-Tracker-Interactive-Map.png.webp" alt="Heat Tracker Map - Houston Public Media" style="padding: 6px;" />
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card mb-3">
+                <div class="card-header">
+                    <a style="text-decoration: none; color:#fff;" href="http://172.21.136.142/presidential-election-map/">Presidential Election Collage Map</a>
+                </div>
+                <div class="row g-0">
+                    <div class="col-md-12">
+                        <a href="http://172.21.136.142/presidential-election-map/"><img src="https://cdn.houstonpublicmedia.org/assets/images/Presindetial-election-2024.png.webp" alt="Presidential Election 2024" style="padding: 6px;" /></a>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
--- web/app/themes/hpmv4/front-page.php
+++ web/app/themes/hpmv4/front-page.php
@@ -87,6 +87,7 @@ $tras = null; ?>
             color:#fff;
             font-weight: bold;
             border-radius: 0px;
+            min-height: 56px;
         }
         .card-header:first-child
         {
@@ -254,7 +255,7 @@ $tras = null; ?>
 					<h2 class="title">
 						<strong>News from <span>NPR</span></strong>
 					</h2>
-					<?php  echo hpm_nprapi_output(); ?>
+					<?php  //echo hpm_nprapi_output(); ?>
 				</div>
 				<div class="col-sm-12 col-lg-4 news-list-right most-view homepage-desktop-gdc hidden">
 					<h2 class="title title-full">
@@ -283,6 +284,6 @@ $tras = null; ?>
 			</div>
 		</section>
 
-        <?php get_template_part("content", "exclusives"); ?>
+        <?php get_template_part("content", "interactives"); ?>
 	</div>
 <?php get_footer(); ?>
