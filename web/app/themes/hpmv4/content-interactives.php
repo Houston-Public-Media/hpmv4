4099b7bada2c90df6118b0f8b19123010bcb7a7a
Smita Patel <sapate34@central.uh.edu>
2024-07-23 12:51:10 -0500

    updated links and image paths for the module

--- web/app/themes/hpmv4/content-interactives.php
+++ web/app/themes/hpmv4/content-interactives.php
@@ -7,11 +7,11 @@
         <div class="col-sm-3">
                 <div class="card mb-3">
                     <div class="card-header">
-                        <a style="text-decoration: none; color:#fff;" href="https://www.houstonpublicmedia.org/articles/news/hurricane/2024/05/30/489073/hurricane-tracker-houston-texas-united-states/">Hurricane & Tropical Strom Tracker</a>
+                        <a style="text-decoration: none; color:#fff;" href="/hurricane-tropical-storm-tracker-texas-houston">Hurricane & Tropical Strom Tracker</a>
                     </div>
                     <div class="row g-0">
                         <div class="col-md-12">
-                            <a href="https://www.houstonpublicmedia.org/articles/news/hurricane/2024/05/30/489073/hurricane-tracker-houston-texas-united-states/"><img src="https://cdn.houstonpublicmedia.org/assets/images/Hurricane-and-storm-tracker-hpm-interactives.png.webp" alt="Hurricane and Tropical Storm Tracker" style="padding: 6px;" /></a>
+                            <a href="/hurricane-tropical-storm-tracker-texas-houston"><img src="https://cdn.houstonpublicmedia.org/assets/images/Hurricane-and-storm-tracker-hpm-interactives.png.webp" alt="Hurricane and Tropical Storm Tracker" style="padding: 6px;" /></a>
 
                         </div>
 
@@ -22,11 +22,11 @@
         <div class="col-sm-3">
             <div class="card mb-3">
                 <div class="card-header">
-                    <a style="text-decoration: none; color:#fff;" href="http://172.21.136.142/texas-power-outage-tracker-map//">Texas Power Outage Tracker Map</a>
+                    <a style="text-decoration: none; color:#fff;" href="/texas-houston-power-outage-tracker-map/">Texas Power Outage Tracker Map</a>
                 </div>
                 <div class="row g-0">
                     <div class="col-md-12">
-                        <a href="http://172.21.136.142/texas-power-outage-tracker-map/"><img src="https://cdn.houstonpublicmedia.org/assets/images/texas-power-outage-tracker-map.png.webp" alt="Texas Power Outage Tracker Map" style="padding: 6px;" /></a>
+                        <a href="/texas-houston-power-outage-tracker-map/"><img src="https://cdn.houstonpublicmedia.org/assets/images/texas-power-outage-tracker-map.png.webp" alt="Texas Power Outage Tracker Map" style="padding: 6px;" /></a>
 
                     </div>
 
@@ -37,11 +37,11 @@
         <div class="col-sm-3">
             <div class="card mb-3">
                 <div class="card-header">
-                    <a style="text-decoration: none; color:#fff;" href="http://172.21.136.142/articles/news/2024/06/11/476089/heat-tracker-2024/">Heat Tracker Map</a>
+                    <a style="text-decoration: none; color:#fff;" href="/houston-weather-temperatures-heat-map/">Heat Tracker Map</a>
                 </div>
                 <div class="row g-0">
                     <div class="col-md-12">
-                        <a href="http://172.21.136.142/articles/news/2024/06/11/476089/heat-tracker-2024/">
+                        <a href="/houston-weather-temperatures-heat-map/">
                             <img src="https://cdn.houstonpublicmedia.org/assets/images/Heat-Tracker-Interactive-Map.png.webp" alt="Heat Tracker Map - Houston Public Media" style="padding: 6px;" />
                         </a>
                     </div>
@@ -53,11 +53,11 @@
         <div class="col-sm-3">
             <div class="card mb-3">
                 <div class="card-header">
-                    <a style="text-decoration: none; color:#fff;" href="http://172.21.136.142/presidential-election-map/">Presidential Election Collage Map</a>
+                    <a style="text-decoration: none; color:#fff;" href="/electoral-college-interactive-map-2024-election">Presidential Election Collage Map</a>
                 </div>
                 <div class="row g-0">
                     <div class="col-md-12">
-                        <a href="http://172.21.136.142/presidential-election-map/"><img src="https://cdn.houstonpublicmedia.org/assets/images/Presindetial-election-2024.png.webp" alt="Presidential Election 2024" style="padding: 6px;" /></a>
+                        <a href="/electoral-college-interactive-map-2024-election"><img src="https://cdn.houstonpublicmedia.org/assets/images/electoral-college-interactive-map-2024.png.webp" alt="Presidential Election 2024" style="padding: 6px;" /></a>
 
                     </div>
 
