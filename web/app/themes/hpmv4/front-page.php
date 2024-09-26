3c505ace6418a05fd24e2c68c3e159ae3a3c50e0
sapate34 <sapate34@central.uh.edu>
2024-09-26 13:32:17 -0500

    Updated election landing page link

--- web/app/themes/hpmv4/front-page.php
+++ web/app/themes/hpmv4/front-page.php
@@ -173,13 +173,20 @@ $tras = null; ?>
 		</section>
 
         <section class="section ads-full text-center">
-            <div class="page-banner" style="display: inline-block;"><a href="/general-election-2024/" title="Election 2024"><picture> <source srcset="https://cdn.houstonpublicmedia.org/assets/images/General-Election-2024-Homepage-Ad-Desktop.png.webp" type="image/webp" media="(max-width: 34em)" /></picture>
-                <picture><source srcset="https://cdn.houstonpublicmedia.org/assets/images/General-Election-2024-Homepage-Ad-Desktop.png" media="(max-width: 34em)" /></picture>
-                <picture><source srcset="https://cdn.houstonpublicmedia.org/assets/images/General-Election-2024-Homepage-Ad-Desktop.png.webp" type="image/webp" media="(max-width: 52.5em)" /></picture>
-                <picture><source srcset="https://cdn.houstonpublicmedia.org/assets/images/General-Election-2024-Homepage-Ad-Desktop.png" media="(max-width: 52.5em)" /></picture>
-                <picture><source srcset="https://cdn.houstonpublicmedia.org/assets/images/General-Election-2024-Homepage-Ad-Desktop.png.webp" type="image/webp" /></picture>
-                <picture><source srcset="https://cdn.houstonpublicmedia.org/assets/images/General-Election-2024-Homepage-Ad-Desktop.png" /></picture>
-                <picture><img src="https://cdn.houstonpublicmedia.org/assets/images/General-Election-2024-Homepage-Ad-Desktop.png" alt="Election 2024" /></picture></a></div>
+
+
+            <div class="page-banner">
+                <a href="/2024-election" title="2024 Election">
+                    <picture>
+                    <source srcset="https://cdn.houstonpublicmedia.org/assets/images/General-Election-2024-Homepage-Ad-Desktop.png.webp" type="image/webp" media="(max-width: 34em)">
+                    <source srcset="https://cdn.houstonpublicmedia.org/assets/images/General-Election-2024-Homepage-Ad-Desktop.png.webp" type="image/webp" media="(max-width: 52.5em)">
+                    <source srcset="https://cdn.houstonpublicmedia.org/assets/images/General-Election-2024-Homepage-Ad-Desktop.png.webp" type="image/webp">
+                    <img decoding="async" src="https://cdn.houstonpublicmedia.org/assets/images/General-Election-2024-Homepage-Ad-Desktop.png" alt="2024 Election">
+                    </picture>
+                </a>
+            </div>
+
+
         </section>
 
 		<section class="section news-list">
