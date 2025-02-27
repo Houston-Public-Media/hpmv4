bb7b3f441f3c6ac98a192b588b3151808e64465a
sapate34 <sapate34@central.uh.edu>
2025-02-27 13:21:21 -0600

    Added Hello Houston Show on the front page.

--- web/app/themes/hpmv4/content-localshows.php
+++ web/app/themes/hpmv4/content-localshows.php
@@ -7,17 +7,17 @@
 		<strong>THIS WEEK on <span>TALK RADIO</span></strong>
 	</h2>
 	<div class="row">
-		<div class="col-sm-6">
+		<div class="col-sm-4">
 			<h3 class="title-style2">
 				<strong>HOUSTON <span>MATTERS</span></strong>
 			</h3>
 			<div class="row">
-				<div class="col-sm-5">
+				<div class="col-sm-4" style="padding-left: 5px; padding-right: 5px;">
 					<div class="image">
 						<a href="/shows/houston-matters/"><img src="https://cdn.houstonpublicmedia.org/wp-content/uploads/2017/07/09164012/HMCC_podcast-tile.png.webp" alt="Houston Matters with Craig Cohen" /></a>
 					</div>
 				</div>
-				<div class="col-sm-7">
+				<div class="col-sm-8" style="padding-left: 5px; padding-right: 5px;">
 					<ul class="list-none news-links" style="margin-top: 0;">
 <?php
 				foreach ( $HMArticles as $ka => $va ) {
@@ -31,17 +31,41 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-sm-6">
+        <div class="col-sm-4">
+            <h3 class="title-style4">
+                <strong>HELLO <span>HOUSTON</span></strong>
+            </h3>
+            <div class="row">
+                <div class="col-sm-4" style="padding-left: 5px; padding-right: 5px;">
+                    <div class="image">
+                        <a href="/shows/hello-houston/"><img src="https://cdn.houstonpublicmedia.org/wp-content/uploads/2017/07/09164012/HMCC_podcast-tile.png.webp" alt="Hello Houston with Ernie Manouse" /></a>
+                    </div>
+                </div>
+                <div class="col-sm-8" style="padding-left: 5px; padding-right: 5px;">
+                    <ul class="list-none news-links" style="margin-top: 0;">
+                        <?php
+                        foreach ( $HMArticles as $ka => $va ) {
+                            $post = $va; ?>
+                            <li style="font-size: 0.9rem;">
+                                <a href="<?php echo get_the_permalink( $post ); ?>"><?php echo get_the_title( $post ); ?></a>
+                            </li>
+                            <?php
+                        } ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+		<div class="col-sm-4">
 			<h3 class="title-style3">
 				<strong>PARTY <span>POLITICS</span></strong>
 			</h3>
 			<div class="row">
-				<div class="col-sm-5">
+				<div class="col-sm-4" style="padding-left: 5px; padding-right: 5px;">
 					<div class="image">
 						<a href="/shows/party-politics/"><img src="https://cdn.houstonpublicmedia.org/wp-content/uploads/2021/09/27123037/PartyPol-21_Podcast-Art_2000x2000.jpg.webp" alt="Party Politics" /></a>
 					</div>
 				</div>
-				<div class="col-sm-7">
+				<div class="col-sm-8" style="padding-left: 5px; padding-right: 5px;">
 					<ul class="list-none news-links" style="margin-top: 0;">
 <?php
 				foreach ( $PPArticles as $ka => $va ) {
@@ -56,5 +80,6 @@
 				</div>
 			</div>
 		</div>
+
 	</div>
 </section>
