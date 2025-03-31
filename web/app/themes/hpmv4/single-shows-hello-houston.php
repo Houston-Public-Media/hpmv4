109a9b1b35ed3ccb023acaf39f5aeba7c3b79bd3
Jared Counts <asentenceismyname@gmail.com>
2025-03-31 11:50:06 -0500

    Tabs instead of spaces Fixed row on Hello Houston page

--- web/app/themes/hpmv4/single-shows-hello-houston.php
+++ web/app/themes/hpmv4/single-shows-hello-houston.php
@@ -43,38 +43,38 @@ get_header(); ?>
 		.show-content > * + * {
 			margin-top: 1rem;
 		}
-        article{
-            padding: 0.5em !important;
-        }
-        article.staff
-        {
-            background-color: #363636;
-            border-bottom: none;
-            color:#fff;
-            /*margin: 10px;*/
-            margin-bottom: 10px;
-        }
-        #search-results article {
-            background-color: #363636!important;
-            border: none;
-            align-items: flex-start !important;
-        }
-         #search-results > h2 {
-            margin: 1rem 0;
-        }
-        article.staff h2, article.staff h2 a
-        {
-            color:#fff !important;
-            text-decoration: none;
-        }
-        article.staff .entry-summary p
-        {
-            color:#fff !important;
-        }
-        .station-social .badges-box
-        {
-            background-color: #c8102e; /*#7787f7  #7787f7 c8102e*/
-        }
+		article{
+			padding: 0.5em !important;
+		}
+		article.staff
+		{
+			background-color: #363636;
+			border-bottom: none;
+			color:#fff;
+			/*margin: 10px;*/
+			margin-bottom: 10px;
+		}
+		#search-results article {
+			background-color: #363636!important;
+			border: none;
+			align-items: flex-start !important;
+		}
+		 #search-results > h2 {
+			margin: 1rem 0;
+		}
+		article.staff h2, article.staff h2 a
+		{
+			color:#fff !important;
+			text-decoration: none;
+		}
+		article.staff .entry-summary p
+		{
+			color:#fff !important;
+		}
+		.station-social .badges-box
+		{
+			background-color: #c8102e; /*#7787f7  #7787f7 c8102e*/
+		}
 		@media screen and (min-width: 34em) {
 			body.single-shows #station-social {
 				display: grid;
@@ -110,97 +110,93 @@ global $post;
 		$show = get_post_meta( $show_id, 'hpm_show_meta', true );
 		$show_title = get_the_title();
 		$show_content = get_the_content();
-        $episodes = HPM_Podcasts::list_episodes( $show_id );
+		$episodes = HPM_Podcasts::list_episodes( $show_id );
 		echo HPM_Podcasts::show_header( $show_id );
 
-        $cat_no = get_post_meta( get_the_ID(), 'hpm_shows_cat', true );
-        $top = get_post_meta( get_the_ID(), 'hpm_shows_top', true );
-        $terms = get_terms( [ 'include'  => $cat_no, 'taxonomy' => 'category' ] );
-        $term = reset( $terms );
-        $ta = 0;
-        $latest_ep_args = [
-            'cat' => 64721,
-            'orderby' => 'date',
-            'order'   => 'DESC',
-            'posts_per_page' => 3,
-            'ignore_sticky_posts' => 1,
-            'post_status' => 'publish',
-            'post_type' => 'post'
-        ];
-        $latest_ep_args['posts_per_page'] = 16;
+		$cat_no = get_post_meta( get_the_ID(), 'hpm_shows_cat', true );
+		$top = get_post_meta( get_the_ID(), 'hpm_shows_top', true );
+		$terms = get_terms( [ 'include'  => $cat_no, 'taxonomy' => 'category' ] );
+		$term = reset( $terms );
+		$ta = 0;
+		$latest_ep_args = [
+			'cat' => 64721,
+			'orderby' => 'date',
+			'order'   => 'DESC',
+			'posts_per_page' => 3,
+			'ignore_sticky_posts' => 1,
+			'post_status' => 'publish',
+			'post_type' => 'post'
+		];
+		$latest_ep_args['posts_per_page'] = 16;
 	}
 
-    ?>
+	?>
 			<div class="houston-matters-page">
 				<div class="about-houston-block">
-                    <?php echo do_shortcode( $post->post_content ); ?>
+					<?php echo do_shortcode( $post->post_content ); ?>
 				</div>
-                <div id="station-social" class="station-social">
-                    <div class="badges-box">
-                        <span class="badge-title">SUBSCRIBE, STREAM &amp; FOLLOW US ON</span>
-                        <?php echo HPM_Podcasts::show_social( $show['podcast'], false, $show_id ); ?>
-                    </div>
-                </div>
+				<div id="station-social" class="station-social">
+					<div class="badges-box">
+						<span class="badge-title">SUBSCRIBE, STREAM &amp; FOLLOW US ON</span>
+						<?php echo HPM_Podcasts::show_social( $show['podcast'], false, $show_id ); ?>
+					</div>
+				</div>
 				<div class="episodes-block" style="padding-top: 15px;">
 					<h2 class="title blue-bar"> <strong><span>MORE Stories</span></strong> </h2>
-                    <div class="row">
-                        <div class="col-sm-6 col-md-4">
-                        </div>
-                        <div class="col-sm-6 col-md-4">
-                        </div>
-
-                        <div class="col-sm-6 col-md-4">
-                            <div class="sidebar-ad">
-                                <h4>Support Comes From</h4>
-                                <div id="div-gpt-ad-1394579228932-1">
-                                    <script type='text/javascript'>
-                                        googletag.cmd.push(function() { googletag.display('div-gpt-ad-1394579228932-1'); });
-                                    </script>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="row">
-                        <?php
-                        global $ka;
-                        $ka = 0;
-                        $tag_ids = [];
-                        $cat = new WP_Query( $latest_ep_args );
-                        $hmcounter = 0;
-                        if ( $cat->have_posts() ) {
-                            while ( $cat->have_posts() ) {
-                                $cat->the_post();
-                                if ( $hmcounter == 2 ) { ?>
-                                    <div class="col-sm-6 col-md-4">
-                                        <div class="sidebar-ad">
-                                            <h4>Support Comes From</h4>
-                                            <div id="div-gpt-ad-1394579228932-1">
-                                                <script type='text/javascript'>
-                                                    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1394579228932-1'); });
-                                                </script>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <?php
-                                } else if ( $hmcounter == 7 ) { ?>
-                                    <div class="col-sm-6 col-md-4">
-                                        <div class="sidebar-ad">
-                                            <h4>Support Comes From</h4>
-                                            <div id="div-gpt-ad-1394579228932-2">
-                                                <script type='text/javascript'>
-                                                    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1394579228932-2'); });
-                                                </script>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <?php
-                                }
-                                get_template_part( 'content', "shows" );
-                                $hmcounter++;
-                            }
-                        } ?>
-                    </div>
+					<div class="row">
+<?php
+						global $ka;
+						$ka = 0;
+						$tag_ids = [];
+						$cat = new WP_Query( $latest_ep_args );
+						$hmcounter = 0;
+						if ( $cat->have_posts() ) {
+							while ( $cat->have_posts() ) {
+								$cat->the_post();
+								if ( $hmcounter == 2 ) { ?>
+									<div class="col-sm-6 col-md-4">
+										<div class="sidebar-ad">
+											<h4>Support Comes From</h4>
+											<div id="div-gpt-ad-1394579228932-1">
+												<script type='text/javascript'>
+													googletag.cmd.push(function() { googletag.display('div-gpt-ad-1394579228932-1'); });
+												</script>
+											</div>
+										</div>
+									</div>
+									<?php
+								} else if ( $hmcounter == 7 ) { ?>
+									<div class="col-sm-6 col-md-4">
+										<div class="sidebar-ad">
+											<h4>Support Comes From</h4>
+											<div id="div-gpt-ad-1394579228932-2">
+												<script type='text/javascript'>
+													googletag.cmd.push(function() { googletag.display('div-gpt-ad-1394579228932-2'); });
+												</script>
+											</div>
+										</div>
+									</div>
+									<?php
+								}
+								get_template_part( 'content', "shows" );
+								$hmcounter++;
+							}
+							if ( $cat->found_posts < 3 ) { ?>
+									<?php if ( $cat->found_posts == 1 ) { ?><div class="col-sm-6 col-md-4"></div><?php } ?>
+									<div class="col-sm-6 col-md-4">
+										<div class="sidebar-ad">
+											<h4>Support Comes From</h4>
+											<div id="div-gpt-ad-1394579228932-1">
+												<script type='text/javascript'>
+													googletag.cmd.push(function() { googletag.display('div-gpt-ad-1394579228932-1'); });
+												</script>
+											</div>
+										</div>
+									</div>
+<?php
+							}
+						} ?>
+					</div>
 
 				</div>
 			</div>
