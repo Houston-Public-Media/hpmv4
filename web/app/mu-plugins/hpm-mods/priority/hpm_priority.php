f3a41bce339541c4f2bfd0f7f04378c285bec39e
jwcounts <jared@jcounts.com>
2026-03-18 11:14:08 -0500

    Switching to WP-Composer from WPackagist Update to WP 6.9.4 Integrating BrightCove video pull into HPM-Priority API output

--- web/app/mu-plugins/hpm-mods/priority/hpm_priority.php
+++ web/app/mu-plugins/hpm-mods/priority/hpm_priority.php
@@ -47,7 +47,9 @@ function hpm_priority_json_list(): WP_HTTP_Response|WP_REST_Response|WP_Error {
 			'type' => '',
 			'link' => ''
 		],
-		'talkshow' => []
+		'talkshow' => [],
+		'weather' => get_transient( 'hpm_weather_api' ),
+		'bcvideos' => hpm_getBrightcovePlaylist()
 	];
 	$indepth_slot = (int)$hpm_priority['inDepthnumber'] - 1;
 	if ( !empty( $hpm_priority['homepage'] ) ) {
@@ -152,14 +154,14 @@ function hpm_priority_json_list(): WP_HTTP_Response|WP_REST_Response|WP_Error {
 		}
 	}
 
-	$weather = get_transient( 'hpm_weather_api' );
-	if ( empty( $weather ) ) {
+	if ( empty( $output['weather'] ) ) {
 		hpm_weather();
-		$weather = get_transient( 'hpm_weather_api' );
+		$output['weather'] = get_transient( 'hpm_weather_api' );
 	}
 
 	if ( $output ) {
-		return rest_ensure_response( [ 'code' => 'rest_api_success', 'message' => esc_html__( 'HPM Priority Homepage Story List', 'hpm-priority' ), 'data' => [ 'articles' => $output['articles'], 'breaking' => $output['breaking'], 'talkshow' => $output['talkshow'], 'weather' => $weather, 'status' => 200 ] ] );
+		$output['status'] = 200;
+		return rest_ensure_response( [ 'code' => 'rest_api_success', 'message' => esc_html__( 'HPM Priority Homepage Story List', 'hpm-priority' ), 'data' => $output ] );
 	} else {
 		return new WP_Error( 'rest_api_sad', esc_html__( 'There has been an error, please try again later.', 'hpm-priority' ), [ 'status' => 500 ] );
 	}
