bc62b24b0801c9650ef0e2d0d47d4aaf76108e62
sapate34 <sapate34@central.uh.edu>
2026-03-30 14:11:15 -0500

    Corrected typos

--- web/app/themes/hpmv4/page-space.php
+++ web/app/themes/hpmv4/page-space.php
@@ -175,7 +175,7 @@ if ( $cat->have_posts() ) {
                             <div class="col-sm-3">
                                 <div class="card mb-3">
                                     <div class="card-header">
-                                        <a style="text-decoration: none; color:#fff;" href="https://www.nasa.gov/missions/artemis/artemis-2/nasa-sets-coverage-for-artemis-ii-moon-mission/">Nasa's Artemis II Coverage</a>
+                                        <a style="text-decoration: none; color:#fff;" href="https://www.nasa.gov/missions/artemis/artemis-2/nasa-sets-coverage-for-artemis-ii-moon-mission/">NASA's Artemis II Coverage</a>
                                     </div>
                                 </div>
                             </div>
