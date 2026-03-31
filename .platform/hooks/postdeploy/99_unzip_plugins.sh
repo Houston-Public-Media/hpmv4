c68fa02c92fac3a019b8ac42db95c766729233e4
jwcounts <jared@jcounts.com>
2026-03-31 13:01:13 -0500

    PHP-FPM config management

--- .platform/hooks/postdeploy/99_unzip_plugins.sh
+++ .platform/hooks/postdeploy/99_unzip_plugins.sh
@@ -5,3 +5,6 @@ chmod -R 777 /var/app/current/web/app/uploads
 rm -rf /var/app/current/web/wp/wp-content/themes/twenty*
 cp /var/app/.env /var/app/current/.env
 sed -i 's/if ( empty( \$author_text ) ) {/if ( empty( $author_text ) \&\& $i->current_author !== null \&\& !is_bool( $i->current_author ) ) {/' /var/app/current/web/app/plugins/co-authors-plus/template-tags.php
+mv /etc/php-fpm.d/www.conf /etc/php-fpm.d/www-old.bak
+mv /etc/php-fpm.d/www-hpm.conf /etc/php-fpm.d/www.conf
+systemctl restart php-fpm.service
