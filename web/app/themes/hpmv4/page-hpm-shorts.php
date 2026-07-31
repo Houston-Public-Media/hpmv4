e77a84e1d39707266f176a3c0b0bdd39092787ed
jwcounts <jared@jcounts.com>
2026-07-31 11:40:39 -0500

    HPM Shorts: update to page code

--- web/app/themes/hpmv4/page-hpm-shorts.php
+++ web/app/themes/hpmv4/page-hpm-shorts.php
@@ -12,7 +12,7 @@ $hasNextPage = ( $offset + $perPage ) < $videos['total']; ?>
 	<style>
 		.btn-primary{ background-color: #237bbd; !important; }
 	</style>
-	<script src="https://players.brightcove.net/<?php echo $options['account_id']; ?>/<?php echo $options['player_id']; ?>_default/index.min.js"></script>
+	<script src="https://players.brightcove.net/<?php echo $options['account_id']; ?>/default_default/index.min.js"></script>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<header class="page-header banner">
@@ -30,7 +30,14 @@ $hasNextPage = ( $offset + $perPage ) < $videos['total']; ?>
 					<div class="col-lg-3 col-md-6 col-12">
 						<div class="card h-100" style="border:none;background:#237bbd;">
 							<img src="<?php echo esc_url( $poster ); ?>" class="card-img-top thumbnail" data-src="<?php echo esc_url( $hlsSource ); ?>" alt="<?php echo esc_html($video['name'] ?? ''); ?>" style="cursor:pointer;">
-							<video class="w-100 d-none" controls playsinline preload="none"></video>
+							<video
+									class="w-100 d-none hpm-video-player"
+									controls
+									playsinline
+									preload="none"
+									data-video-id="<?php echo esc_attr($video['id'] ?? ''); ?>"
+									data-video-name="<?php echo esc_attr($video['name'] ?? ''); ?>"
+							></video>
 							<div class="card-body">
 								<h6 class="card-title mb-0 text-white">
 									<?php echo esc_html($video['name'] ?? ''); ?>
@@ -59,26 +66,107 @@ $hasNextPage = ( $offset + $perPage ) < $videos['total']; ?>
 	</div>
 	<script>
 		document.addEventListener("DOMContentLoaded", function(){
-			const thumbnails = document.querySelectorAll(".thumbnail");
-			thumbnails.forEach(function(img){
-				img.addEventListener("click", function(){
-					document.querySelectorAll("video").forEach(function(v){
-						v.pause();
-						v.currentTime = 0;
-						v.classList.add("d-none");
-						if (v.previousElementSibling) {
-							v.previousElementSibling.classList.remove("d-none");
-						}
-					});
-					const video = this.nextElementSibling;
-					const src = this.dataset.src;
-					if (!src) return;
-					video.src = src;
-					this.classList.add("d-none");
-					video.classList.remove("d-none");
-					video.play();
+			const analyticsBaseURL =  window.location.protocol + "//metrics.brightcove.com/tracker/v2/?";
+			const session = Math.floor(Math.random() * 1000000) + "_" + new Date().toISOString();
+			const pageURL = encodeURIComponent(window.location.href);
+			const referrer = encodeURIComponent(document.referrer);
+			const ACCOUNT_ID = "<?php echo esc_js($options['account_id']); ?>";
+			let currentVideo = null;
+			let firstTimeUpdate = true;
+			let initialPosition = 0;
+			let lastPosition = 0;
+			function sendData(url){
+				const img = document.createElement("img");
+				img.src = url;
+				img.style.display = "none";
+				document.body.appendChild(img);
+			}
+			function sendAnalyticsEvent(eventType, videoElement, extra={}){
+				if(!currentVideo){
+					return;
+				}
+				let url = analyticsBaseURL +
+					"event=" + eventType +
+					"&session=" + session +
+					"&domain=houstonpublicmedia.org" +
+					"&account=" + ACCOUNT_ID +
+					"&time=" + Date.now() +
+					"&destination=" + pageURL;
+				if(referrer){
+					url += "&source=" + referrer;
+				}
+				url += "&video=" + encodeURIComponent(currentVideo.id);
+				url += "&video_name=" +  encodeURIComponent(currentVideo.name);
+				if(eventType === "video_engagement"){
+					url += "&video_duration=" +  videoElement.duration;
+					url += "&range=" + extra.range;
+				}
+				//console.log("Brightcove Analytics:" +url);
+				sendData(url);
+			}
+			function setupVideo(video){
+				let started = false;
+				video.addEventListener("play", function(){
+					if(!started){
+						started = true;
+						sendAnalyticsEvent( "video_view",  video );
+					}
 				});
-			});
+				video.addEventListener("loadedmetadata", function(){
+					sendAnalyticsEvent(
+						"video_impression",
+						video
+					);
+				});
+				video.addEventListener("timeupdate", function(e){
+					let position = video.currentTime;
+					if(firstTimeUpdate){
+						initialPosition = position;
+						lastPosition = position;
+						firstTimeUpdate = false;
+					}
+					if( Math.floor(position) - Math.floor(lastPosition) >= 10 ){
+						let range = initialPosition +  ".." + position;
+						sendAnalyticsEvent( "video_engagement",  video,
+							{
+								range: range
+							}
+						);
+						lastPosition = position;
+					}
+				});
+			}
+			document.querySelectorAll(".thumbnail")
+				.forEach(function(img){
+					img.addEventListener("click", function(){
+						document.querySelectorAll(".hpm-video-player")
+							.forEach(function(v){
+								v.pause();
+								v.currentTime = 0;
+								v.classList.add("d-none");
+								if(v.previousElementSibling){
+									v.previousElementSibling.classList.remove("d-none");
+								}
+							});
+						const video = this.nextElementSibling;
+						const src = this.dataset.src;
+						currentVideo = {
+							id: video.dataset.videoId,
+							name: video.dataset.videoName
+						};
+						if(!src){
+							return;
+						}
+						video.src = src;
+						this.classList.add("d-none");
+						video.classList.remove("d-none");
+						firstTimeUpdate = true;
+						initialPosition = 0;
+						lastPosition = 0;
+						setupVideo(video);
+						video.play();
+					});
+				});
 		});
 	</script>
 <?php get_footer(); ?>
